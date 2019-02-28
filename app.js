55238608bff288a35fc4fc5fba8f879535d56dda
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-02-28 14:12:39 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+// Fibonacci数列の関数
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n == 1){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+// iまで計算
+const length = 40;
+for (let i = 1; i <= length; i++){
+    console.log(fib(i));
+}
