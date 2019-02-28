2acd70779196cfbf60362db3f99758a2c01c9a90
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-02-28 14:16:41 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 'use strict';
 // Fibonacci数列の関数
+// 再帰すると指数オーダーになる。
+// function fib(n) {
+//     if (n === 0) {
+//         return 0;
+//     } else if (n == 1){
+//         return 1;
+//     }
+//     return fib(n-1) + fib(n-2);
+// }
+// Mapを使った省力化
+const memo = new Map();
+memo.set(0, 0); // n=0のとき
+memo.set(1, 1); // n=1のとき
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n == 1){
-        return 1;
+    if(memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n-1) + fib(n-2);
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 // iまで計算
 const length = 40;
